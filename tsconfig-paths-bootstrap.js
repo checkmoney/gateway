a77cea890f737ef7f419a285b5968e79348904d5
Igor Kamyshev <igor@kamyshev.me>
2020-05-10 14:42:58 +0700

    chore: fix migration issues

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -3,7 +3,8 @@ const tsConfigPaths = require('tsconfig-paths');
 
 const { paths } = require('./tsconfig.json').compilerOptions;
 
+const baseUrl = resolve(__dirname, 'dist');
 tsConfigPaths.register({
-  baseUrl: __dirname,
+  baseUrl,
   paths,
 });
